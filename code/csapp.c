936f0c9709eeaff5d6624265e7c7e365664bf782
Refrain <3244156674@qq.com>
2024-02-29 22:38:22 +0800

    1

---
+++ code/csapp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
+ 
+ void unix_error(char *msg) /* Unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+pid_t Fork(void)
+{
+    pid_t pid;
+
+    if ((pid = fork()) < 0)
+        unix_error("Fork error");
+    return pid;
+ }
