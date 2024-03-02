52d1bb00dcac658d519f6193a93d8a72f31d9f3d
0AyanamiRei <3244156674@qq.com>
2024-03-02 18:42:55 +0800

    1

--- code/mycsapp.c
+++ code/mycsapp.c
@@ -8,11 +8,32 @@
 #include <getopt.h>
 #include <signal.h>
 #include <asm/signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 /* Misc constants */
-#define	MAXLINE	 8192  /* Max text line length */
-#define MAXBUF   8192  /* Max I/O buffer size */
-#define LISTENQ  1024  /* Second argument to listen() */
+#define MAXLINE 8192 /* Max text line length */
+#define MAXBUF 8192  /* Max I/O buffer size */
+#define LISTENQ 1024 /* Second argument to listen() */
 
 extern char **environ;
 
@@ -26,10 +47,13 @@ ssize_t Sio_putl(long v);
 ssize_t Sio_puts(char s[]);
 void Sio_error(char s[]);
 unsigned int Sleep(unsigned int secs);
-
-
-
-
+pid_t Waitpid(pid_t pid, int *iptr, int options);
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
+void Sigemptyset(sigset_t *set);
+void Sigfillset(sigset_t *set);
+void Sigaddset(sigset_t *set, int signum);
+void Sigdelset(sigset_t *set, int signum);
+int Sigismember(const sigset_t *set, int signum);
 
 /* 实现函数 */
 
@@ -39,22 +63,22 @@ void app_error(char *msg) /* Application error */
     exit(0);
 }
 
-char *Fgets(char *ptr, int n, FILE *stream) 
+char *Fgets(char *ptr, int n, FILE *stream)
 {
     char *rptr;
 
     if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
-	app_error("Fgets error");
+        app_error("Fgets error");
 
     return rptr;
 }
 
-pid_t Fork(void) 
+pid_t Fork(void)
 {
     pid_t pid;
 
     if ((pid = fork()) < 0)
-	unix_error("Fork error");
+        unix_error("Fork error");
     return pid;
 }
 
@@ -64,39 +88,41 @@ void unix_error(char *msg) /* Unix-style error */
     exit(0);
 }
 
-void Kill(pid_t pid, int signum) 
+void Kill(pid_t pid, int signum)
 {
     int rc;
 
     if ((rc = kill(pid, signum)) < 0)
-	unix_error("Kill error");
+        unix_error("Kill error");
 }
 
 static void sio_reverse(char s[])
 {
     int c, i, j;
 
-    for (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+    for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
+    {
         c = s[i];
         s[i] = s[j];
         s[j] = c;
     }
 }
 
-static void sio_ltoa(long v, char s[], int b) 
+static void sio_ltoa(long v, char s[], int b)
 {
     int c, i = 0;
     int neg = v < 0;
 
     if (neg)
-	v = -v;
+        v = -v;
 
-    do {  
-        s[i++] = ((c = (v % b)) < 10)  ?  c + '0' : c - 10 + 'a';
+    do
+    {
+        s[i++] = ((c = (v % b)) < 10) ? c + '0' : c - 10 + 'a';
     } while ((v /= b) > 0);
 
     if (neg)
-	s[i++] = '-';
+        s[i++] = '-';
 
     s[i] = '\0';
     sio_reverse(s);
@@ -119,7 +145,7 @@ ssize_t sio_puts(char s[])
 ssize_t sio_putl(long v)
 {
     char s[128];
-    
+
     sio_ltoa(v, s, 10);
     return sio_puts(s);
 }
@@ -127,24 +153,24 @@ ssize_t sio_putl(long v)
 void sio_error(char s[])
 {
     sio_puts(s);
-    _exit(1);                                   
+    _exit(1);
 }
 
 ssize_t Sio_putl(long v)
 {
     ssize_t n;
-  
+
     if ((n = sio_putl(v)) < 0)
-	sio_error("Sio_putl error");
+        sio_error("Sio_putl error");
     return n;
 }
 
 ssize_t Sio_puts(char s[])
 {
     ssize_t n;
-  
+
     if ((n = sio_puts(s)) < 0)
-	sio_error("Sio_puts error");
+        sio_error("Sio_puts error");
     return n;
 }
 
@@ -153,43 +179,96 @@ void Sio_error(char s[])
     sio_error(s);
 }
 
-unsigned int Sleep(unsigned int secs) 
+unsigned int Sleep(unsigned int secs)
 {
     unsigned int rc;
 
     if ((rc = sleep(secs)) < 0)
-	unix_error("Sleep error");
+        unix_error("Sleep error");
     return rc;
 }
 
+pid_t Waitpid(pid_t pid, int *iptr, int options)
+{
+    pid_t retpid;
+
+    if ((retpid = waitpid(pid, iptr, options)) < 0)
+        unix_error("Waitpid error");
+    return (retpid);
+}
+
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+{
+    if (sigprocmask(how, set, oldset) < 0)
+        unix_error("Sigprocmask error");
+    return;
+}
+
+void Sigemptyset(sigset_t *set)
+{
+    if (sigemptyset(set) < 0)
+        unix_error("Sigemptyset error");
+    return;
+}
+
+void Sigfillset(sigset_t *set)
+{
+    if (sigfillset(set) < 0)
+        unix_error("Sigfillset error");
+    return;
+}
+
+void Sigaddset(sigset_t *set, int signum)
+{
+    if (sigaddset(set, signum) < 0)
+        unix_error("Sigaddset error");
+    return;
+}
+
+void Sigdelset(sigset_t *set, int signum)
+{
+    if (sigdelset(set, signum) < 0)
+        unix_error("Sigdelset error");
+    return;
+}
+
+int Sigismember(const sigset_t *set, int signum)
+{
+    int rc;
+    if ((rc = sigismember(set, signum)) < 0)
+        unix_error("Sigismember error");
+    return rc;
+}
 
 /* 函数解析 */
-// 1
+// 1 waitpid
 /* waitpid(-1, &status, 0)参数解释:
-*   pid = -1: wait set 由所有子进程组成
-*   &status:  将导致子进程返回的状态信息给status
-*   options = 0 : 父进程被挂起,直到其wait set中的一个子进程终止
-*   waitpid如果成功则返回子进程的PID, 其他错误返回-1或0
-*  
-*   解释status的宏
-*   WIFEXITED(status): 子进程通过exit或者return正常终止, 返回true
-*   WEXITSTATUS(status): 返回一个正常终止的子进程的退出状态 (exit(?)中的?)
-*   
-*   解释错误条件
-*   wait set 为空集, 再调用waitpid就返回-1, 并设置errno为ECHILD
-*   waitpid被signal中断, 返回-1, 并设置errno为EINTR
-*   检查errno的值可以得知是否回收完当前进程的子进程
-*/ 
+ *   pid = -1: wait set 由所有子进程组成
+ *   &status:  将导致子进程返回的状态信息给status
+ *   options = 0 : 父进程被挂起,直到其wait set中的一个子进程终止
+ *   waitpid如果成功则返回子进程的PID, 其他错误返回-1或0
+ *
+ *   解释status的宏
+ *   WIFEXITED(status): 子进程通过exit或者return正常终止, 返回true
+ *   WEXITSTATUS(status): 返回一个正常终止的子进程的退出状态 (exit(?)中的?)
+ *
+ *   解释错误条件
+ *   wait set 为空集, 再调用waitpid就返回-1, 并设置errno为ECHILD
+ *   waitpid被signal中断, 返回-1, 并设置errno为EINTR
+ *   检查errno的值可以得知是否回收完当前进程的子进程
+ */
 
-
-// 2
+// 2 parseline(自定义)
 /* int parseline(char *buf, char **argv):
-*   解析cmdline的函数,返回值0或1,根据最后一个参数是否为'&'决定任务是否在后台运行
-*   '&', return (bg = 1):background = 1
-*   
-*   解析的参数会传给argv这个二重指针
-*/ 
+ *   解析cmdline的函数,返回值0或1,根据最后一个参数是否为'&'决定任务是否在后台运行
+ *   '&', return (bg = 1):background = 1
+ *
+ *   解析的参数会传给argv这个二重指针
+ */
 
-
-
-
+// 3 kill
+/* int kill(pid_t pid, int sig);
+ *   if pid > 0, 发送信号sig给进程pid
+ *   if pid = 0, 发送信号sig给调用kill函数所在进程组中的每个进程
+ *   if pid < 0, 发送信号sig给进程|pid|所在进程组中的每个进程
+ */
--- practice/8_8_signalprob.c
+++ practice/8_8_signalprob.c
@@ -1,4 +1,16 @@
 #include "/home/refrain/csapp/Csapp/code/mycsapp.c"
+/* Question:
+    
+    Tell me what will input this program? 
+    it's 213 but not 212, let me tell you why:
+    简单来说,父进程将信号SIGUSR1传给子进程后,子进程通过handler1这个信号处理程序捕获并且处理信号
+    在handler1中输出--counter, 这个counter是子进程自己的变量所以输出1
+
+    对于父进程中的输出,在父进程中,输出了counter和++counter,这是属于父进程的变量,所以输出是2和3
+    由于在输出3之前调用了Waitpid(-1, NULL, 0), 父进程被挂起,等待子进程处理信号输出1后再输出3
+    所以顺序是2-1-3
+*/
+
 
 volatile long counter = 2;
 
@@ -8,7 +20,7 @@ void handler1(int sig)
 
     Sigfillset(&mask);
     Sigprocmask(SIG_BLOCK, &mask, &prev_mask); /* Block sigs */
-    Sio_putl(--counter);
+    Sio_putl(--counter); /* 打印long类型变量 1 */
     Sigprocmask(SIG_SETMASK, &prev_mask, NULL); /* Restore sigs */
 
     _exit(0);
@@ -19,23 +31,26 @@ int main()
     pid_t pid;
     sigset_t mask, prev_mask;
 
-    printf("%ld", counter);
+    printf("%ld", counter); /* input 2 */
     fflush(stdout);
 
+    /* SIGUSR1: 默认行为=终止, 用户自定义的信号1*/
     signal(SIGUSR1, handler1);
-    if ((pid = Fork()) == 0)
+    if ((pid = Fork()) == 0) /* Child process */
     {
         while (1)
         {
         };
     }
-    Kill(pid, SIGUSR1);
+    Kill(pid, SIGUSR1); /* Father process will send the signal to child process */
     Waitpid(-1, NULL, 0);
 
     Sigfillset(&mask);
     Sigprocmask(SIG_BLOCK, &mask, &prev_mask); /* Block sigs */
-    printf("%ld", ++counter);
+    printf("%ld", ++counter); /* input 3*/
     Sigprocmask(SIG_SETMASK, &prev_mask, NULL); /* Restore sigs */
 
     exit(0);
 }
+
+/* output 213 */
