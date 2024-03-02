31f3ef71ec61856a53b2d9bc1f199a74fbe7074c
0AyanamiRei <3244156674@qq.com>
2024-03-02 15:40:11 +0800

    1

--- practice/8_7_snoozeProg.c
+++ practice/8_7_snoozeProg.c
@@ -7,6 +7,18 @@
     Slept for 3 of 5 secs.
     > ···
 */
+
+/* Mission Accomplished：
+    以防第一遍读不明白到底要让我们干什么，我建议你应该实际运行一下调用snooze函数后按Ctrl+C会发生什么。
+
+    并没有得到sleep剩下的时间，而是直接退出了程序
+
+
+
+
+*/
+
+
 #include "/home/refrain/csapp/Csapp/code/chapter-8/ECF/mycsapp.c"
 
 /* 顺带学习一下: 函数指针 */
@@ -20,33 +32,37 @@ sighandler_t handler = sigint_handler;
 
 int main(int argc, char *argv[], char *envp[])
 {/* Just try!! */
-    int time = atoi(argv[1]);
-    printf("Now your PC will sleep about %ds, you can enter Ctrl+C to wake up it!\n", time);
-    sleep(time);
-
     signal(SIGINT, handler);
 
-    printf("Now time is going\n");
-    return 0;
-}
+    int time = atoi(argv[1]);
+    printf("Now your PC will sleep about %ds, you can enter Ctrl+C to wake up it!\n", time);
+    snooze(time);
 
-void sigint_handler(int sig){
-    printf("We got the signal: SIGINT!\nSo, bye!\n");
+    /* If we take it here, can it be usefule? */
+    /* signal(SIGINT, handler); */
+    /* Answer is : No! */
+
+    printf("Now time is going\n");
     exit(0);
 }
 
-unsigned int snooze(unsigned int secs){
-    int selpt_secs = secs - sleep(secs);
-    printf("Slept for %d of %d secs.\n", selpt_secs, secs);
-    return secs - selpt_secs;
+void sigint_handler(int sig){
+    printf("\nWe got the signal: SIGINT!\n");
+    printf("···· We should wait for the snooze return.\n");
 }
 
+unsigned int snooze(unsigned int secs){
 
+    int selpt_secs = secs - sleep(secs);
+
+    printf("Slept for %d of %d secs.\n", selpt_secs, secs);
+
+    return secs - selpt_secs;
+}
 
 
 /* How??
     在收到I/O键盘的输入信号 Ctrl+C的时候,程序收到一个k=2 SIGINT的信号默认行为为终止
     程序不会输出Slept for %d of 5 secs.这段话,更不会看到Now time is going, 所以我们要做的
     就是改变这个信号的默认行为,
-
 */
