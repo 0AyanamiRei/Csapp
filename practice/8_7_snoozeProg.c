d515a05e1f7e82d397b94edb9ee85f3d551c7054
0AyanamiRei <3244156674@qq.com>
2024-03-02 15:11:37 +0800

    1

--- practice/8_7_snoozeProg.c
+++ practice/8_7_snoozeProg.c
@@ -8,3 +8,45 @@
     > ···
 */
 #include "/home/refrain/csapp/Csapp/code/chapter-8/ECF/mycsapp.c"
+
+/* 顺带学习一下: 函数指针 */
+typedef void (*sighandler_t)(int);
+
+void sigint_handler(int sig);
+unsigned int snooze(unsigned int secs);
+
+/* 顺带学习一下: 函数指针 */
+sighandler_t handler = sigint_handler;
+
+int main(int argc, char *argv[], char *envp[])
+{/* Just try!! */
+    int time = atoi(argv[1]);
+    printf("Now your PC will sleep about %ds, you can enter Ctrl+C to wake up it!\n", time);
+    sleep(time);
+
+    signal(SIGINT, handler);
+
+    printf("Now time is going\n");
+    return 0;
+}
+
+void sigint_handler(int sig){
+    printf("We got the signal: SIGINT!\nSo, bye!\n");
+    exit(0);
+}
+
+unsigned int snooze(unsigned int secs){
+    int selpt_secs = secs - sleep(secs);
+    printf("Slept for %d of %d secs.\n", selpt_secs, secs);
+    return secs - selpt_secs;
+}
+
+
+
+
+/* How??
+    在收到I/O键盘的输入信号 Ctrl+C的时候,程序收到一个k=2 SIGINT的信号默认行为为终止
+    程序不会输出Slept for %d of 5 secs.这段话,更不会看到Now time is going, 所以我们要做的
+    就是改变这个信号的默认行为,
+
+*/
