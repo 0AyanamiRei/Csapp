454af59ced4dc9459670300aebbe6c1bae96e105
0AyanamiRei <3244156674@qq.com>
2024-03-05 13:51:56 +0800

    1

--- notes/chapter-8/homework/Myshell_8_26.c
+++ notes/chapter-8/homework/Myshell_8_26.c
@@ -35,23 +35,29 @@
 #define MAXLINE 128
 #define TRUE 1
 #define FALSE 0
+#define LEN 64
 
-typedef struct job
+typedef struct Job
 {
     pid_t pid;
     pid_t pgid;
     int argc;
     char state; /*stopped or running*/
     char **argv;
-    job *next;   /*point to next job*/
-}job;
+    Job *next;   /*point to next job*/
+}Job;
 
 /* global 变量*/
-job* bg_head = NULL;
+Job* bg_head = NULL;
+Job *fg_job;
+Job *tmp_jobs;
+pid_t ppid;
 
 
-/* 添加一个job到bg */
-void add_job();
+/* job's linklist function */
+void add_job(Job* bg_head, Job* job);
+void delete_job(Job* bg_head, pid_t id);
+Job* search_job(Job* bg_head, pid_t id);
 
 
 typedef void (*sighandler_t)(int);
@@ -62,6 +68,7 @@ extern **environ;
 void unix_error(char *msg);
 pid_t Fork(void);
 sighandler_t Signal(int signum, sighandler_t handler);
+void Free(void* ptr);
 
 
 
@@ -69,10 +76,25 @@ sighandler_t Signal(int signum, sighandler_t handler);
 void eval(char *cmdline);
 int parseline(char *buf, char **argv);
 int builtin_command(char **argv);
+int jobs(char **argv);
+int bg(char **argv);
+int fg(char **argv);
+
 
 
 /* signal handler function*/
 void SIGCHLD_handler(int sig);
+void SIGINT_handler(int sig);
+void SIGTSTP_handler(int sig);
+void SIGUSR1_handler(int sig);
+
+
+/* test function */
+void foo(char **argv);
+
+void pf_wait(int bg){
+    if(!bg) printf("Now, we are waitting for the fg job accomplished.\n");
+}
 
 
 
@@ -90,24 +112,26 @@ void SIGCHLD_handler(int sig);
     这个时候就必须要全局的数据结构去存放这种信息了,这个问题我们留给设计数据结构,也就是问题二中继续讨论.
 
 
-
-
-
   解决第二个问题, 如何设计数据结构去管理后台的作业才恰当?
     我打算实现一个链表去动态的管理后台作业, 根据copilot的建议,如果能够将一个作业的信息传递给信号处理函数,那么
-    我们就能区分前台与后台SIGCHLD的问题了.
+    我们就能区分前台与后台SIGCHLD的问题了. 查阅书上关于waitpid的内容 "如果wait set中的一个进程在刚调用的时刻就已经终止了
+    那么waitpid立即返回终止的子进程的pid", 根据pid就可以在全局的数据结构中找到其进程的所有信息,所以我们在SIGCHLD的
+    处理函数中使用wait set既做到回收已终止子进程又可以获取终止子进程的信息.
+    ***当我们获取到来自前后台作业结束而发出的SIGCHLD信号后, 我们可以用kill(pid_t pid, int sig)向父进程发送信号来解除
+    挂起的状态, 因此我们要获取父进程的PID, 同样使用全局变量去存储这个信息
 
-
-
-
+  解决第三个问题, 对于Ctrl+Z和Ctrl+C信号按kernel->shell->fg的顺序发送有什么简便的地方?
+    对于我们使用的shell, 上述两个信号会由内核直接发送给fg_job, 当子进程发生结束,停止,继续的行为时都会向父进程发送
+    SIGCHLD信号,这为SIGCHLD信号处理函数带来了很大的不便.
 
 */
 
 
 int main(int argc, char **argv, char **envp) {
+    bg_head = (Job*)malloc(sizeof(Job));
     char cmdline[MAXLINE]; /* Command line */
+    ppid = getppid();
 
-    
     do{ 
         printf("Ciallo~(∠・ω< )⌒☆ > ");
         Fgets(cmdline, MAXLINE, stdin);
@@ -115,21 +139,10 @@ int main(int argc, char **argv, char **envp) {
             exit(0);
         }
         eval(cmdline);
-
-        /* 等待: 1,fg job完成 2,SIGINT信号 3,SIGTSTP信号 */
-        sigset_t mask;
-        sigfillset(&mask);
-        sigsuspend(mask); 
-
     } while(TRUE);
 
     exit(0);
 }
-
-
-
-
-
 
 
 
@@ -156,6 +169,10 @@ sighandler_t Signal(int signum, sighandler_t handler)
         unix_error("signal error");
 }
 
+void Free(void *ptr){
+    free(ptr);
+    ptr = NULL;
+}
 
 /* main function */
 
@@ -172,6 +189,7 @@ void eval(char *cmdline)
     if (argv[0] == NULL)
         return;
 
+    /* builtin_command return 0 表明可能是一个可执行程序 */
     if (!builtin_command(argv)) {
         if ((pid = Fork()) == 0) {
             if (execve(argv[0], argv, environ) < 0) {
@@ -181,23 +199,18 @@ void eval(char *cmdline)
     }
 
     if (!bg) {
-        int status;
-        if (waitpid(pid, &status, 0) < 0)
-            unix_error("waitfg: waitpid error");
+        /* 等待: 1,fg job完成 2,SIGINT信号 3,SIGTSTP信号 */
+        pf_wait(bg);
+        sigset_t mask;
+        sigfillset(&mask);
+        sigdelset(&mask, SIGCHLD); /* 通过信号处理函数让识别前台job还是后台job */
+        sigsuspend(mask); 
     }
     else
+        pf_wait(bg);
         printf("%d %s", pid, cmdline);
     }
     return;
-}
-
-int builtin_command(char **argv)
-{
-    if (!strcmp(argv[0], "quit"))
-        exit(0);
-    if (!strcmp(argv[0], "&"))
-        return 1;
-    return 0;
 }
 
 int parseline(char *buf, char **argv)
@@ -231,16 +244,136 @@ int parseline(char *buf, char **argv)
     return bg;
 }
 
+int builtin_command(char **argv)
+{
+    if(!strcmp(argv[0], "quit"))
+    {
+        exit(0);/* 结束shell进程 */
+    } else if(!strcmp(argv[0], "&"))
+    {
+        return 1; /* 无效的 */
+    } else if(!strcmp(argv[0], "jobs"))
+    {
+        jobs(argv);
+    } else if(!strcmp(argv[0], "bg"))
+    {
+
+    } else if(!strcmp(argv[0], "fg"))
+    {
+
+    } else if(!strcmp(argv[0], "foo"))
+    {
+        foo(argv);
+    } else
+    { /* 这是一个可执行程序 */
+        return 0;
+    }
+}
+
+int jobs(char **argv){
+    ;
+}
+
+int bg(char **argv){
+
+}
+
+int fg(char **argv){
+    ;
+}
 
 
 /* signal handler function*/
 
 void SIGCHLD_handler(int sig){
     char check_bg = TRUE; /* 依赖全局数据结构的信息得到前后台进程 */
-    if(check_bg) { 
-        ;
+    int status = 0;
+    /* 收到该信号意味着有子进程终止了,由父进程调用处理函数 */
+    pid_t pid = waitpid(-1, &status, WNOHANG);
+
+    if(pid == fg_job->pid)
+        check_bg = FALSE;
+
+    if(!check_bg) { /* 前台作业 */
+        if(WIFEXITED(status)){ /* 正常退出(注意没有编写exit或return的子进程默认exit(0)) */
+            printf("fg child %d terminated by status %d\n", pid, status);
+        }
+        else if (WIFSIGNALED(status)) {
+            char buf[LEN];
+            sprintf(buf, "fg child %d terminated by signal %d", pid, WTERMSIG(status));
+            psignal(WTERMSIG(status), buf);
+        }else {
+            printf("fg child %d terminated abnormally\n", pid);
+        }
+        Free(fg_job); /* 最好是对前台作业清空状态 */
+        kill(ppid, SIGUSR1); /* 发送信号给shell,解除sigsuspend状态 */
     }
-    else {
-        ;
+    else { /* 后台作业 */
+        delete_job(bg_head, pid);
+        if(WIFEXITED(status)){
+            printf("bg child %d terminated by status %d\n", pid, status);
+        }
+        else if (WIFSIGNALED(status)) {
+            char buf[LEN];
+            sprintf(buf, "bg child %d terminated by signal %d", pid, WTERMSIG(status));
+            psignal(WTERMSIG(status), buf);
+        }else {
+            printf("bg child %d terminated abnormally\n", pid);
+        }
     }
+}
+
+void SIGUSR1_handler(int sig){/* 这个信号仅仅提醒shell前台作业完成, 什么都不需要做 */
+    ;
+}
+
+void SIGINT_handler(int sig){
+    ;
+}
+
+void SIGTSTP_handler(int sig){
+    ;
+}
+
+
+/* job's linklist function */
+void add_job(Job* bg_head, Job* job){
+    if(bg_head->next == NULL) {
+        bg_head->next = job;
+        return;
+    }
+    job->next = bg_head->next;
+    bg_head->next = job;
+}
+
+void delete_job(Job* bg_head, pid_t id){
+    Job* cur = bg_head;
+    while (cur->next != NULL) {
+        if (cur->next->pid == id) {
+            Job* to_delete = cur->next;
+            cur->next = to_delete->next;
+            free(to_delete);
+            return;
+        }
+        cur = cur->next;
+    }
+}
+
+Job* search_job(Job* bg_head, pid_t id) {
+    Job* current = bg_head->next;
+    while (current != NULL) {
+        if (current->pid == id) {
+            return current;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+
+
+/* test function */
+void foo(char **argv){
+    sleep(5);
+    exit(123);
 }
--- notes/chapter-8/homework/jobs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Start two background jobs
-sleep 100
-
-# List all background jobs
